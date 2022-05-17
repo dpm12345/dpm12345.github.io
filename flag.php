d8fbd6d1e418319d6abde84c62a4416a135ab22e
dpm12345 <1006975692@qq.com>
2022-05-17 20:26:01 +0800

    Site updated: 2022-05-17 20:26:00

---
+++ flag.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @Author       : zhong
+ * @since        : 2022-05-17 20:23:16
+ * @LastAuthor   : Please set LastEditors
+ * @lastTime     : 2022-05-17 20:23:16
+ */
+
+
+
+
+header("http://127.0.0.1/flag.php");
