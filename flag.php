692e1aa4b4481e6a4451a7de7466f0e5c1b4cc6c
dpm12345 <1006975692@qq.com>
2022-05-17 20:32:58 +0800

    Site updated: 2022-05-17 20:32:57

--- flag.php
+++ flag.php
@@ -3,10 +3,10 @@
  * @Author       : zhong
  * @since        : 2022-05-17 20:23:16
  * @LastAuthor   : Please set LastEditors
- * @lastTime     : 2022-05-17 20:23:16
+ * @lastTime     : 2022-05-17 20:32:22
  */
 
 
 
 
-header("http://127.0.0.1/flag.php");
+header("Location:http://127.0.0.1/flag.php");
