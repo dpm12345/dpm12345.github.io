8558a978cf786f01a2750ab6fc9e832b3af6ac0b
dpm12345 <1006975692@qq.com>
2026-01-22 09:44:47 +0000

    Site updated: 2026-01-22 17:44:44

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1769069731128";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1769075053506";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
